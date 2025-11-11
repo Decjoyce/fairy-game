07875d135a35cbf01c7f1a2ef6db9c1257ee7799
Funnymike09 <mike.nip@mail.ru>
2025-11-11 10:16:24 +0500

    Carcas for enemy stuff

---
+++ EnemyStateMachine/NodeState.gd
@@ -0,0 +1,11 @@
+class_name NodeState
+extends Node
+
+func on_process(delta : float):
+	pass
+
+func enteer():
+	pass
+
+func exit():
+	pass
---
+++ EnemyStateMachine/idle_state.gd
@@ -0,0 +1,13 @@
+extends NodeState
+
+@export var Body : StaticBody3D # for shmoovment
+@export var Sprite : Sprite3DBillBoard #For animation control i hope
+
+func on_process(delta : float):
+	pass
+
+func enteer():
+	pass
+
+func exit():
+	pass
