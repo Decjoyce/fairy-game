9b97b92a05ea77cc74cfe3c2c66e177d9b5eeda0
Funnymike09 <mike.nip@mail.ru>
2025-11-24 07:24:53 +0500

    confuse

--- PathingStar/a_star.gd
+++ PathingStar/a_star.gd
@@ -12,3 +12,21 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	grid_map.do_debug_path(start_tile, end_tile)
+
+func _on_remove_tile_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		grid_map.set_cell_item(test_tile, grid_map.walkable_items[0], 0) # stone floor tile, walkable
+	else:
+		grid_map.set_cell_item(test_tile, 2, 0) # 2 is a wall
+	grid_map.setup_astar_grid(grid_map.walkable_items)
+
+func _on_add_wood_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		grid_map.walkable_items = [0,1] # stone and wood floor tile
+	else:
+		grid_map.walkable_items = [0] # just stone floor tile
+	grid_map.setup_astar_grid(grid_map.walkable_items)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action("ui_cancel"):
+		get_tree().quit()
