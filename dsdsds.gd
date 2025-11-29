5a169c7c45ae272f17a4ebb2a91ee5742df6f576
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2025-11-29 14:41:41 +0000

    ll

--- dsdsds.gd
+++ dsdsds.gd
@@ -2,15 +2,10 @@ extends Node3D
 
 @export var anim_pl: AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var end_screen: PackedScene
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func to_menu():
+	get_tree().change_scene_to_packed(end_screen)
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.owner is PlayerTest:
