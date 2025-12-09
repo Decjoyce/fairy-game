ddad6723b53991307ce528448e2372551f5c0831
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2025-12-09 23:47:53 +0000

    reload current scene

--- scenes/__prototyping/debugger/debug.gd
+++ scenes/__prototyping/debugger/debug.gd
@@ -30,8 +30,12 @@ func _process(delta: float) -> void:
 			$Console.console_line.release_focus()
 			$Console.visible = false
 			Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
+			
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if Input.is_action_just_pressed("reset_vs"):
+		get_tree().reload_current_scene()
 
 func _init_me() -> void:
 	player = get_tree().get_first_node_in_group("Player") as PlayerTest
