55560de4a8b181ca11828eda74194d64abc26356
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2025-12-31 16:30:42 +0000

    chacharealsmooth

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -1,30 +1,30 @@
 extends Node
 
 @onready var anim_player: AnimationPlayer = $Camera3D/AnimationPlayer
-@onready var fade_anim: AnimationPlayer = $FadeLayer/AnimationPlayer
+#@onready var fade_anim: AnimationPlayer = $FadeLayer/AnimationPlayer
 
 var game_started := false
 
 @export var vs_scnee: PackedScene
 
-func _ready():
-	anim_player.play("CameraSlow")
+#func _ready():
+	#anim_player.play("CameraSlow")
 	#Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
 
 func _process(delta):
 	if game_started:
 		return
 	
-	if Input.is_action_just_pressed("ui_StartButton"):
-		game_started = true
-		start_game()
+	#if Input.is_action_just_pressed("ui_StartButton"):
+#		game_started = true
+	#	start_game()
 
 func start_game():
 	anim_player.play("SpeedIntoCavee") 
 
 	await anim_player.animation_finished
 
-	fade_anim.play("FadetoBlack")
-	await fade_anim.animation_finished
+	#fade_anim.play("FadetoBlack")
+	#await fade_anim.animation_finished
 
 	get_tree().change_scene_to_packed(vs_scnee)
---
+++ scenes/MainMenu/helpme.gd
@@ -0,0 +1,36 @@
+extends Camera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	pass # Replace with function body.
+
+var is_free: bool
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("free_mouse"):
+		is_free = !is_free
+		if is_free: Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else: Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	
+	#if is_free: return
+	var z_input = Input.get_axis("turn_left", "turn_right")
+	
+	var x_input = Input.get_axis("move_left", "move_right")
+	
+	var y_input = Input.get_axis("move_down", "move_up")
+	$Y_ROT.position.y += y_input * delta
+	$Y_ROT.position.x += x_input * delta
+	$Y_ROT.position.z += z_input * delta
+	
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and !is_free:
+		$Y_ROT.rotate_y(-event.relative.x * mouse_sensitivity)
+		$Y_ROT/X_ROT.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Y_ROT/X_ROT.rotation.x = clampf($Y_ROT/X_ROT.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+
+var mouse_sensitivity = 0.002
