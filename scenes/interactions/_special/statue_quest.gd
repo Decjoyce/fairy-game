bd5dd92e739a7d56b2dd63f669626ee750affe20
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2026-03-17 23:36:18 +0000

    statue_saving

--- scenes/interactions/_special/statue_quest.gd
+++ scenes/interactions/_special/statue_quest.gd
@@ -1,3 +1,4 @@
+class_name StatueQuest
 extends Node3D
 
 signal on_piece_inserted(piece: String)
@@ -9,35 +10,43 @@ var stages: int = 3 # 0 = default, 1 = 1st piece added, 2 = 2nd piece added, 3 =
 var current_stage: int
 
 @export var statue: Sprite3DBillBoard
+@export var itm_receiver: StatueReceiver
 
-func insert_piece(num_received: int, item_received: Grabbable_Item) -> void:
+func insert_piece(num_received: int, item_received: Grabbable_Item, emit_sigs: bool = true) -> void:
 	current_stage = num_received
 	print(item_received.keywords)
 	match item_received.keywords:
-		"BASE": piece_inserted_base()
-		"HEAD": piece_inserted_head()
-		"ARM": piece_inserted_arm()
+		"BASE": piece_inserted_base(emit_sigs)
+		"HEAD": piece_inserted_head(emit_sigs)
+		"ARM": piece_inserted_arm(emit_sigs)
 		_: printerr(item_received, "contains invalid keyword for statue quest = ", item_received.keywords, " || Must be either BASE, HEAD, or ARM")
 	
-	on_stage_completed.emit(current_stage)
-	on_stage_completed_sig.emit(current_stage/stages)
+	if emit_sigs:
+		on_stage_completed.emit(current_stage)
+		on_stage_completed_sig.emit(float(current_stage/stages))
 	
 	if current_stage != 3: return
 	quest_completed()
 
+func update_graphics() -> void:
+	match current_stage:
+		0: pass
+		1: piece_inserted_base(false)
+		2: piece_inserted_arm(false)
+		3: piece_inserted_head(false)
 
-func piece_inserted_base() -> void:
-	on_piece_inserted.emit("BASE")
+func piece_inserted_base(emit_sigs: bool = true) -> void:
+	if emit_sigs: on_piece_inserted.emit("BASE")
 	statue.animation = 1
 
-func piece_inserted_head() -> void:
-	on_piece_inserted.emit("HEAD")
+func piece_inserted_head(emit_sigs: bool = true) -> void:
+	if emit_sigs: on_piece_inserted.emit("HEAD")
 	statue.animation = 3
 
-func piece_inserted_arm() -> void:
-	on_piece_inserted.emit("ARM")
+func piece_inserted_arm(emit_sigs: bool = true) -> void:
+	if emit_sigs: on_piece_inserted.emit("ARM")
 	statue.animation = 2
 
-func quest_completed() -> void:
-	on_quest_completed.emit(1)
+func quest_completed(emit_sigs: bool = true) -> void:
+	if emit_sigs: on_quest_completed.emit(1)
 	
