22b5dc8a00437717c641d071f0a992b76b0d6f42
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2026-03-19 21:41:37 +0000

    Update item_giver.gd

--- scenes/interactions/_receivers/wip_item_giver/item_giver.gd
+++ scenes/interactions/_receivers/wip_item_giver/item_giver.gd
@@ -3,5 +3,5 @@ extends Interactable
 
 @export var item_to_give: Grabbable_Item
 
-func begin_interact(sig: float = -1) -> void:
+func begin_interact(sig: float = -1, hand: PlayerHand = null) -> void:
 	pass
