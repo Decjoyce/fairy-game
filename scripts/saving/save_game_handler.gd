3feedce1323458e8107aaa832e94bb3f37a78304
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2026-03-25 10:14:36 +0000

    menu loading func

--- scripts/saving/save_game_handler.gd
+++ scripts/saving/save_game_handler.gd
@@ -40,7 +40,7 @@ func load_game() -> void:
 	if is_loading: return
 	var saved_game: SavedGame = load("user://savegame.tres") as SavedGame
 	
-	EffectsPlayer.blur_out(1, 1, true)
+	EffectsPlayer.blur_out(1, 1, 0, true)
 	EffectsPlayer.saturize(0, 0.5, -1,true)
 	
 	is_loading = true
@@ -70,9 +70,44 @@ func load_game() -> void:
 		else: # Nodes that were originally placed in the scene
 			if uid_list[obj.uid].has_method("on_load_game"):
 				uid_list[obj.uid].on_load_game(obj)
-	EffectsPlayer.blur_out(0, 1, true)
+	EffectsPlayer.blur_out(0, 1, 1, true)
 	EffectsPlayer.saturize(1, 1, 0, true)
 	is_loading = false
+
+func load_game_from_menu(scene_to_load: PackedScene) -> void:
+	if is_loading: return
+	var saved_game: SavedGame = load("user://savegame.tres") as SavedGame
+	
+	is_loading = true
+	await load_scene_for_first_time(scene_to_load)
+	
+	get_tree().call_group("Player", "on_before_load_game") 
+	get_tree().call_group("Player", "on_load_game", saved_game.player_data) 
+	
+	on_before_load_game.emit()
+	get_tree().call_group("PO", "on_before_load_game")
+	
+	uid_list.clear()
+	for i in get_tree().get_nodes_in_group("PO"):
+		assert(i is PersistentObject, "A node was marked as PO despite not being of type PersistentObject -- NOTIFY DECLAN ASAP")
+		if i.get_parent().has_meta("uid"):
+			uid_list[i.get_uid()] = i
+	
+	#print(uid_list)
+	
+	for obj in saved_game.saved_data:
+		if !uid_list[obj.uid]:
+			printerr("AN UID - Write A PROPER ERROR BRO - in SAVEGAMEHANDLER")
+			continue
+		if obj.uid == "[NONNATIVE]": # Nodes that were not placed in scene but spawned in after 
+			_load_non_native_objs(obj)
+		else: # Nodes that were originally placed in the scene
+			if uid_list[obj.uid].has_method("on_load_game"):
+				uid_list[obj.uid].on_load_game(obj)
+	EffectsPlayer.blur_out(0, 1, 1, true)
+	EffectsPlayer.saturize(1, 1, 0, true)
+	is_loading = false
+
 
 func _load_non_native_objs(obj: SavedData) -> void:
 	#prints(obj.uid, obj.scene_path)
@@ -86,6 +121,14 @@ func _load_non_native_objs(obj: SavedData) -> void:
 
 func reload_cur_scene() -> void:
 	get_tree().reload_current_scene()
+	await get_tree().node_added
+	world_root = get_tree().current_scene
+	if !world_root.is_node_ready():
+		await world_root.ready
+
+func load_scene_for_first_time(scene_to_change_to: PackedScene) -> void:
+	print("hey")
+	get_tree().change_scene_to_packed(scene_to_change_to)
 	await get_tree().node_added
 	world_root = get_tree().current_scene
 	if !world_root.is_node_ready():
