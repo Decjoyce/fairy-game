c639c760fda26e9d39f46c768244419b8458c3fd
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2026-04-21 20:27:23 +0100

    fixing

---
+++ scripts/input_context_helper.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var joy_dict: Dictionary[String, JoyIcons]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(Input.get_joy_name(0))
+
+
+func update_me() -> void:
+	pass
