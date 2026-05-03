25340a306e83050980925f6ab18b83d89bc9f73d
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2026-05-03 19:13:54 +0100

    opti pt.3

--- scenes/_levels/Final_Puzzles/Tutorials/statue_section.gd
+++ scenes/_levels/Final_Puzzles/Tutorials/statue_section.gd
@@ -5,6 +5,6 @@ extends Node3D
 @export var baby_scene: PackedScene
 
 func statue_finished(sig: float = -1.0) -> void:
-	return
+	#return
 	var babyscene : Node3D = baby_scene.instantiate() as Node3D
 	instantiate_point.add_child(babyscene)
