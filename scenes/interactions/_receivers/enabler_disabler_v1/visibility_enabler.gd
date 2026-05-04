22d5425540c1ac9a5c1fc78c237c0adb8c189707
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2026-05-04 17:47:20 +0100

    pushing small changes before i push big change

--- scenes/interactions/_receivers/enabler_disabler_v1/visibility_enabler.gd
+++ scenes/interactions/_receivers/enabler_disabler_v1/visibility_enabler.gd
@@ -3,16 +3,20 @@ extends Node
 @export var linked_node: Node3D
 
 func enable_obj(sig: float) -> void:
+	if !linked_node: return
 	linked_node.visible = true
 	print("Ensbale")
 
 func disable_obj(sig: float) -> void:
+	if !linked_node: return
 	linked_node.visible = false
 	print("Disbale")
 
 func toggle_visibilty(sig: float) -> void:
+	if !linked_node: return
 	linked_node.visible = !linked_node.visible
 	print(linked_node.visible)
 
 func disable_obj_using_area(obj: Object) -> void:
+	if !linked_node: return
 	linked_node.queue_free()
