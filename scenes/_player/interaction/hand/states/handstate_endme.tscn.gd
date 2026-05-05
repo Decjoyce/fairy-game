7d1f5e13e543f47d42b7a9ecc0cbcbfcab966d5a
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2026-05-05 02:11:27 +0100

    baby section done and ready for audio

--- scenes/_player/interaction/hand/states/handstate_endme.tscn.gd
+++ scenes/_player/interaction/hand/states/handstate_endme.tscn.gd
@@ -9,8 +9,20 @@ func enter(previous_state_path: String, data := {}) -> void:
 	hand_controller.input_controls.visible = false
 	hand_controller.animation_player.play("end_anim")
 	if hand_controller.hand_type == 0: 
-		reset_pos = Vector2(player_interact.size.x/2.75, player_interact.size.y/2)
-	else: reset_pos = Vector2(player_interact.size.x - (player_interact.size.x/2.4), player_interact.size.y/2)
+		reset_pos = Vector2((player_interact.size.x/2.2) - (hand_controller.size.x/2), player_interact.size.y/2)
+	else: reset_pos = Vector2((player_interact.size.x - (player_interact.size.x/2.2)) - (hand_controller.size.x/2), player_interact.size.y/2)
 	
 	var tween := get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_LINEAR)
-	tween.tween_property(hand_controller, "global_position", reset_pos, 0.1)
+	tween.tween_property(hand_controller, "global_position", reset_pos, 0.15)
+	await get_tree().create_timer(1).timeout
+	if hand_controller.hand_type == 0: 
+		reset_pos = Vector2((player_interact.size.x/2.4) - (hand_controller.size.x/2), player_interact.size.y/2)
+	else: reset_pos = Vector2((player_interact.size.x - (player_interact.size.x/2.4)) - (hand_controller.size.x/2), player_interact.size.y/2)
+	tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property(hand_controller, "global_position", reset_pos, 0.15)
+	await get_tree().create_timer(1.05).timeout
+	if hand_controller.hand_type == 0: 
+		reset_pos = Vector2((player_interact.size.x/2.2) - (hand_controller.size.x/2), player_interact.size.y/2.7)
+	else: reset_pos = Vector2((player_interact.size.x - (player_interact.size.x/2.4)) - (hand_controller.size.x/2), player_interact.size.y/2)
+	tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property(hand_controller, "global_position", reset_pos, 0.15)
--- scripts/audio_sig_receiver.gd
+++ scripts/audio_sig_receiver.gd
@@ -4,13 +4,17 @@ extends Node
 @export var audio_player: AudioStreamPlayer3D
 
 func enable_audio_via_sig(sig:float) -> void:
+	if !audio_player: return
 	audio_player.play()
 
 func disable_audio_via_sig(sig:float) -> void:
+	if !audio_player: return
 	audio_player.stop()
 
 func enable_audio_via_obj(obj:Object) -> void:
+	if !audio_player: return
 	audio_player.play()
 
 func disable_audio_via_obj(obj:Object) -> void:
+	if !audio_player: return
 	audio_player.stop()
