a8a11676a45cfe2500570617d4a84dcd344ca646
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2026-05-05 12:47:51 +0100

    Bridge Arrow Trap now trigs after loading

--- scenes/_levels/Final_Puzzles/Mechanical_Puzzles/special_timer.gd
+++ scenes/_levels/Final_Puzzles/Mechanical_Puzzles/special_timer.gd
@@ -1,7 +1,12 @@
 extends Timer
 
+@export var new_wait_time: float
+
 func start_me(sig: float) -> void:
 	start()
 
 func stop_me(sig: float) -> void:
 	stop()
+
+func set_wait_time_to_new() -> void:
+	wait_time = new_wait_time
