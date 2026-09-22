dc1a1ff195d643f568fa3a91c7d0ce863d9dd7ce
Funnymike09 <mike.nip@mail.ru>
2026-09-22 05:51:57 +0500

    Purge of old stuff
    Mostly sounds but also some scenes or scripts with combat

--- scenes/combat/attack_sequence.gd
+++
@@ -1,4 +0,0 @@
-class_name AttackSequence
-extends Resource
-
-@export var attack_order: Array[AttackData]
--- zzz_GARBAGE/OBSELETE__g_grid_database.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func check_grid_space(cell_to_check: Vector3) -> bool:
-	return false
